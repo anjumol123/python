017705684dd37c8eb7a2b8d5d09925da2e5ee21c
anjumol123 <144668871+anjumol123@users.noreply.github.com>
2023-11-14 12:14:52 +0530

    Add files via upload

---
+++ program39.py
@@ -0,0 +1,15 @@
+a=open("demo.txt","r")
+b=open("t","w")
+c=a.readlines()
+d=len(c)
+for i in range(0,d):
+    if i%2==0:
+      b.write(c[i])
+    else:
+         pass
+b.close()
+b=open("t","r")
+c=b.read()
+print(c)
+a.close()
+b.close()
